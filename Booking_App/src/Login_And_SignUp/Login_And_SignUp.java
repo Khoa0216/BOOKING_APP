967b4c1660da00a3469def7676540698b00f0e0c
Khoa0216 <pnk1622005@gmail.com>
2025-04-27 21:09:18 +0700

    Update: MenuForm
    thêm code để truy cập vào menuform

--- Booking_App/src/Login_And_SignUp/Login_And_SignUp.java
+++ Booking_App/src/Login_And_SignUp/Login_And_SignUp.java
@@ -47,6 +47,40 @@ public class Login_And_SignUp {
         }
     }
     
-    
+    public static String checkLoaiDN(String username, String password) {
+        String query = "SELECT BOOKING_APP.DOANHNGHIEP.LOAIDN"
+                    + " FROM BOOKING_APP.NGUOIDUNG JOIN BOOKING_APP.DOANHNGHIEP"
+                    + " ON BOOKING_APP.NGUOIDUNG.ID=BOOKING_APP.DOANHNGHIEP.ID"
+                    + " WHERE EMAIL = ? AND MATKHAU = ?"; //câu lệnh query SQL, ? là nơi sẽ thay thế giá trị vào. (*)
+
+        
+        // cấu trúc try-with-resources
+        /*
+            try (resource res=...; resource res2=...){
+            // sử dụng tài nguyên.
+            }
+            Lợi ích: không cần phải gọi close() tài nguyên thủ công sau khi sử dụng (dù lỗi hay không).
+        */
+        try (Connection conn = OracleDataBase_Connection.getConnection("nguoidung_user", "12345678");
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            // thiết lập các tham số trong câu lệnh SQL (*)
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+
+            // thực thi câu lệnh và lấy kết quả
+            ResultSet rs = stmt.executeQuery();
+
+            // nếu có kết quả trả về, lấy loại tài khoản
+            if (rs.next()) { // .next() giống như con trỏ, trỏ đến từng dòng của câu lệnh query, khi còn kết quả trỏ sẽ trả về true, ngược lại là false.
+                return rs.getString("LOAIDN");
+            } else {
+                return null; // nếu không có kết quả, nghĩa là đăng nhập thất bại
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     
 }
