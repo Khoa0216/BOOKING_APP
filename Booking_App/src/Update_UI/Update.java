981971b893f7728862de3aa64894f54a22fef115
kienboi <164771048+kienboi@users.noreply.github.com>
2025-05-05 21:41:38 +0700

    Add files via upload

---
+++ Booking_App/src/Update_UI/Update.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Update_UI;
+
+/**
+ *
+ * @author bobui
+ */
+public class Update {
+    public static void main(String[] args){
+        
+    }
+}
